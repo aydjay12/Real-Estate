f0a8dd5926345aa3c3a072b226b526aa75fd945f
aydjay12 <aydjay12@gmail.com>
2025-02-01 11:21:26 +0100

    fixed booking and favourites page

--- client/src/pages/Bookings/Bookings.jsx
+++ client/src/pages/Bookings/Bookings.jsx
@@ -9,9 +9,8 @@ import UserDetailContext from "../../context/UserDetailContext";
 const Bookings = () => {
   const { data, isError, isLoading } = useProperties();
   const [filter, setFilter] = useState("");
-  const {
-    userDetails: { bookings },
-  } = useContext(UserDetailContext);
+  const { userDetails } = useContext(UserDetailContext);
+  const bookings = userDetails?.bookings || [];
 
   if (isError) {
     return (
@@ -34,28 +33,28 @@ const Bookings = () => {
       </div>
     );
   }
+
   return (
     <div className="wrapper">
       <div className="flexColCenter paddings innerWidth properties-container">
         <SearchBar filter={filter} setFilter={setFilter} />
 
         <div className="paddings flexCenter properties">
-          {
-            data
-              .filter((property) =>
-                bookings.map((booking) => booking.id).includes(property.id)
+          {data
+            .filter((property) =>
+              (bookings.map((booking) => booking.id) || []).includes(
+                property.id
               )
-
-              .filter(
-                (property) =>
-                  property.title.toLowerCase().includes(filter.toLowerCase()) ||
-                  property.city.toLowerCase().includes(filter.toLowerCase()) ||
-                  property.country.toLowerCase().includes(filter.toLowerCase())
-              )
-              .map((card, i) => (
-                <PropertyCard card={card} key={i} />
-              ))
-          }
+            )
+            .filter(
+              (property) =>
+                property.title.toLowerCase().includes(filter.toLowerCase()) ||
+                property.city.toLowerCase().includes(filter.toLowerCase()) ||
+                property.country.toLowerCase().includes(filter.toLowerCase())
+            )
+            .map((card, i) => (
+              <PropertyCard card={card} key={i} />
+            ))}
         </div>
       </div>
     </div>
